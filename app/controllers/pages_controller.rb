64b0e88fd9453267e1bd19564a75602ed96cc83b
Moises Marinho <moisesmarinhosilva87@gmail.com>
2022-03-14 11:49:47 -0300

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:index]
 
-  def home
+  def index
+  end
+
+  def show
+    slug = params[:slug]
+    view_file = Rails.root.join('app', 'views', 'pages', "#{slug}.html.erb")
+
+    unless view_file.exist?
+      render 'pages/404', status: 404
+      return
+    end
+
+    render "pages/#{slug}"
   end
 end
