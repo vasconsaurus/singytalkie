31274f58ec8c5b742acf32f45a6d9a15a30d9d16
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-14 08:38:50 -0300

    add dynamic routes to pages controller and route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,16 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def show
+    slug = params[:slug]
+    view_file = Rails.root.join('app', 'views', 'pages', "#{slug}.html.erb")
+
+    unless view_file.exist?
+      render 'pages/404', status: 404
+      return
+    end
+
+    render "pages/#{slug}"
+  end
 end
