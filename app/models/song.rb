ae2a84fcd323cf5be4a54ff444058008bd9e4014
Fabio Gardinal <fabio.gardinal@gmail.com>
2022-03-14 15:57:48 -0300

    logica resolvida

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ApplicationRecord
+  LEVELS = {:beginner => 10, :intermediate => 20, :advanced => 35}
   has_many :bookmarks
   validates :title,  presence: true
   validates :lyrics, presence: true
@@ -7,10 +8,27 @@ class Song < ApplicationRecord
   validates :genre, presence: true
   validates :translation, presence: true
 
+  def complete_song(level)
+    sentences = lyrics.split("\n")
+    split_sentences = sentences.map(&:split)
+    selected_words = []
+    LEVELS[level].times do
+      word = ""
+      until word.size >= 3 && !selected_words.include?(word)
+        sentence_index = rand(0..(split_sentences.length - 1))
+        current_sentence = split_sentences[sentence_index]
+        next if current_sentence.empty?
 
-  def complete_song
-    words = lyrics.split(" ")
-    kword = words.select { |word| word.size >= 3 }.sample
-    lyrics.gsub(kword, "*" * kword.size)
+        word_index = rand(0..(split_sentences[sentence_index].length - 1))
+        word = split_sentences[sentence_index][word_index]
+      end
+      selected_words << word
+      split_sentences[sentence_index][word_index] = "[INPUT]"
+    end
+    split_sentences.map { |sentence| sentence.join(" ") }.join("\n")
   end
 end
+
+
+    #   kword = words.select { |word| word.size >= 3 }.sample
+    #   lyric_changed.gsub(kword, "*" * kword.size)
