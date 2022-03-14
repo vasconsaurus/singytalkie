508a1a4243a6acd6f00635631fddfe2f093eb2a5
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-14 11:44:52 -0300

    add random song button

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   resources :songs, only: %i[show index]
 
+  get 'random', to: 'songs#random', as: :random
   get ':slug', to: 'pages#show', as: 'pages'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
