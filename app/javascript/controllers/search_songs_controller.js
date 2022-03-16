600696efd191b8ad7646e8b1b617907001ca76b1
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-16 18:50:31 -0300

    songs index loads empty

--- app/javascript/controllers/search_songs_controller.js
+++ app/javascript/controllers/search_songs_controller.js
@@ -3,13 +3,24 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
 
-  connect() {
-    console.log(this.formTarget)
-    console.log(this.inputTarget)
-    console.log(this.listTarget)
-  }
+  // update(event) {
+  //   const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+  //   fetch(url, { headers: { "Accept": "text/plain" } })
+  //     .then(response => response.text())
+  //     .then((data) => {
+  //       this.listTarget.outerHTML = data
+  //     })
+  // }
 
   update(event) {
-    console.log("TODO: send request in AJAX")
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`;
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.containerTarget.innerHTML = data;
+      });
+    }, 200)
   }
 }
