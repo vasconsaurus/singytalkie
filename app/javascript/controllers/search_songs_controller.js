06e2424d43b7e684621d8a333b1f9a9b79d94b97
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-03-17 14:40:53 -0300

    try to fix serch

--- app/javascript/controllers/search_songs_controller.js
+++ app/javascript/controllers/search_songs_controller.js
@@ -3,24 +3,13 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["form", "input", "list"]
 
-  // update(event) {
-  //   const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
-  //   fetch(url, { headers: { "Accept": "text/plain" } })
-  //     .then(response => response.text())
-  //     .then((data) => {
-  //       this.listTarget.outerHTML = data
-  //     })
-  // }
-
   update(event) {
-    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`;
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      fetch(url, { headers: { 'Accept': 'text/plain' } })
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    fetch(url, { headers: { "Accept": "text/plain" } })
       .then(response => response.text())
       .then((data) => {
-        this.containerTarget.innerHTML = data;
-      });
-    }, 200)
+        this.listTarget.outerHTML = data
+      })
   }
+
 }
